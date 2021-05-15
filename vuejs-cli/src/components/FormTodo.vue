05b629eaac833e879b2de25995059268cb78de73
itchecosta <itchecosta@gmail.com>
2021-05-14 21:37:15 -0300

    código aula

---
+++ vuejs-cli/src/components/FormTodo.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="form-todo form-group">
+          <p>
+              <input placeholder="nome" type="text" class="form-control" name="author" v-model="name">
+          </p>
+          <p>
+              <textarea placeholder="Comentário" name="message" id="message" class="form-control" v-model="message"></textarea>
+          </p>
+          <button v-on:click="addComment" type="submit" class="btn btn-primary">Comentar</button>
+      </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+      return {
+          comments: [],
+          name: '',
+          message: ''
+      }
+  },
+  methods: {
+      addComment() {
+          if (this.message.trim() === '') {
+              return;
+          }
+
+          this.$emit('add-todo', {
+             name: this.name,
+             message: this.message
+          })
+
+          this.name = '';
+          this.message = '';
+      }
+  }
+}
+</script>
